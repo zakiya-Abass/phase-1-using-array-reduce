b403d050e86b5da52e57563bcf84213b405fd18e
Zakiya-Abass <zakiyaabass@icloud.com>
2023-01-16 20:14:06 +0300

    array reduce

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+let totalBatteries = batteryBatches.reduce(function (batteries, numbers) {
+  return batteries + numbers;
+});
+console.log(totalBatteries);
